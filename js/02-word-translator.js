8e68ea7a65a3b90cc63c2bf4a9cfef78d2b9af97
nghihai <nghi84072@gmail.com>
2023-10-02 17:39:39 -0700

    word-translator

---
+++ js/02-word-translator.js
@@ -0,0 +1,12 @@
+//Input 
+let code = prompt("Please enter a language code: ")
+
+//Check for the condition 
+if (code == 'es' || code == 'de'|| code == 'en'|| code == 'fr')
+{
+    alert("Hello World translated in French is: Bonjour le monde.")
+}
+else
+{
+    alert("Hello World.")
+}
