e80f7429e31995d1794e016709db4bf3f37a82f7
nghihai <nghi84072@gmail.com>
2023-10-02 17:34:53 -0700

    larger_or_smaller

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,15 @@
+//Input 
+let num1 = prompt("Please enter num1: ")
+let num2 = prompt("Please enter num2: ")
+
+//Check which number is larger
+if (num1 > num2){
+    alert(num1 + " is larger.")
+}
+else if (num2 > num1)
+{
+    alert(num2 + " is larger.")
+}
+else{
+    alert("The two numbers are equal.")
+}
